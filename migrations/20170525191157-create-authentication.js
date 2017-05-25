7373c4d5b58f92c4361e069ce15d417ffe87f125
HowDoIGitHelp <bbadoodles@gmail.com>
2017-05-26 03:33:45 +0800

    change schema, add with password

---
+++ migrations/20170525191157-create-authentication.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Authentications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Authentications');
+  }
+};
