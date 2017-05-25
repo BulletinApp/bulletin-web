4e4a88f33f1fdd3c413113ef35bb3978d8aa01bd
MiriamMarieTy <mariethegreat89303@gmail.com>
2017-05-26 04:08:17 +0800

    add login page

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     models.BulletinEvent.findAll().then(events =>{
-        
+
         res.render('index', { events });
     });
 });
@@ -23,6 +23,10 @@ router.get('/events', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
     res.render('register', { title: 'Nigguh'});
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'Nigguh'});
 });
 
 module.exports = router;
