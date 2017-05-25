5121be3efe2cff884a449996de3f2c13383b0baa
deneiruy <deneiruy@binyiruyax.localadmin>
2017-05-26 02:31:09 +0800

    add init for datepicker

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -2,5 +2,11 @@ $(document).ready(function(){
     $('.carousel').carousel();
     // $('.carousel.carousel-slider').carousel({fullWidth: true});
 
-      $('.parallax').parallax();
+    $('.parallax').parallax();
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 100, // Creates a dropdown of 15 years to control year
+        max: new Date()
+    });
 });
