7d8c8b8a99686d51b8729cdadd1606bb3f485361
Deneir Uy <deneiruy@gmail.com>
2017-05-28 18:46:22 +0800

    check if fields are valid

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,16 +1,80 @@
-$(document).ready(function() {
-    $("#password_confirm").keyup(validate);
+var org_name = $('#org_name'),
+    date_established = $('#date_established'),
+    photo = $('#photo'),
+    password = $('#password'),
+    password_confirm = $('#password_confirm'),
+    contact_person = $('#contact_person'),
+    contact_number = $('#contact_number'),
+    email = $('#email'),
+    description = $('#description');
+var isComplete = true;
+
+$('#password, #password_confirm').on('keyup', function () {
+    if (password.val() == password_confirm.val()) {
+        password_confirm.removeClass('invalid').addClass('validate');
+        isComplete = true;
+    } else {
+        password_confirm.addClass('invalid').removeClass('validate');
+        isComplete = false;
+    }
 });
 
-function validate() {
-    var password1 = $("#password").val();
-    var password2 = $("#password_confirm").val();
+$('#password, #org_name, #photo, #contact_person, #contact_number, #email, #description').on('focus', function () {
+    $(':focus').removeClass('invalid').addClass('validate');
+});
 
+date_established.on('focus', function () {
+    $(':focus').removeClass('invalid');
+});
 
-    if(password1 == password2) {
-        $("#validate-status").text("true");
+$('#submit').click(function (e) {
+    if (org_name.val() == '') {
+        org_name.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
     }
-    else {
-        $("#validate-status").text("false");
+
+    if (date_established.val() == '') {
+        date_established.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
     }
-}
+
+    if (photo.val() == '') {
+        photo.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (password.val() == '') {
+        password.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (password_confirm.val() == '') {
+        password_confirm.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (contact_person.val() == '') {
+        contact_person.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (contact_number.val() == '') {
+        contact_number.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (email.val() == '') {
+        email.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (description.val() == '') {
+        description.addClass('invalid').removeClass('validate');
+        isComplete = false; // or e.preventdefault();
+    }
+
+    if (isComplete == false) {
+        $('#error_msg').html("error");
+        return false;
+    }
+});
