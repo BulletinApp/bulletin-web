d56ee6e4b5d0d6a3e989bb66c95300e26d20414e
Deneir Uy <deneiruy@gmail.com>
2017-05-28 14:53:21 +0800

    initial commit

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -6,6 +6,7 @@ function validate() {
     var password1 = $("#password").val();
     var password2 = $("#password_confirm").val();
 
+
     if(password1 == password2) {
         $("#validate-status").text("true");
     }
